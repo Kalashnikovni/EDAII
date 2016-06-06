922e1ac609b04e5a21b37c2b289fcf2973111623
N. Soncini <soncininicolas@gmail.com>
2016-06-05 22:32:23 -0300

    {SeqList.hs} Minor changes of spacing to be consistent with other files

--- SeqList.hs
+++ SeqList.hs
@@ -1,6 +1,7 @@
 import Par
 import Seq
 
+
 --TABULATE
 tabulateL :: (Int -> a) -> Int -> [a]
 tabulateL f n 
@@ -8,17 +9,20 @@ tabulateL f n
     | n > 0  = let (a,b) = f n ||| (tabulateL f (n-1))
                in appendS b [a]
 
+
 --MAP
 mapL :: (a -> b) -> [a] -> [b]
 mapL _ []     = []
 mapL f (x:xs) = let (a,b) = f x ||| (map f xs)
                 in a:b 
 
+
 --FILTER
 filterL :: (a -> Bool) -> [a] -> [a]
 filterL _ [] = []
 filterL f (x:xs) = let (a,b) = f x ||| (filter f xs)
                    in if a then x:b else b
+
 
 --SHOWT
 showtL :: [a] -> TreeView a [a]
@@ -29,12 +33,14 @@ showtL xs  = let
                 (l, r) = (takeS xs n) ||| (dropS xs n)
              in NODE l r
 
+
 --CONCAT
 concatL :: [[a]] -> [a]
 concatL []          = []
 concatL [xs]        = xs
 concatL (xs:ys:zss) = let (a,b) = (appendS xs ys) ||| (concatL zss)
                       in appendS a b
+
 
 --REDUCE
 reduceL :: (a -> a -> a) -> a -> [a] -> a
@@ -51,6 +57,7 @@ reduceL' :: (a -> a -> a) -> [a] -> a
 reduceL' _ [x]      = x 
 reduceL' f [x,y]    = f x y
 reduceL' f xs       = reduceL' f (contraer f xs)
+
 
 --SCAN
 expandir :: (a -> a -> a) -> [a] -> [a] -> [a]
@@ -70,6 +77,8 @@ scanL' f n xs  = expandir f xs (scanL' f n (contraer f xs))
 showlL :: [a] -> ListView a [a]
 showlL []     = NIL
 showlL (x:xs) = CONS x xs
+
+
 
 instance Seq [] where
     emptyS       = []
